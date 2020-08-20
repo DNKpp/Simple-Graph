57baf72ea2926439309a2368bb71f7d0ef61f08b
DNKpp <DNKpp2011@gmail.com>
2020-08-20 20:40:42 +0200

    remove old astar and dijkstra test cases
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,11 @@
-#define CATCH_CONFIG_MAIN 
+//          Copyright Dominic Koepke 2019 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include "catch.hpp"
 
-#include "../include/Simple-Graph/algorithm.hpp"
+#include "Simple-Graph/algorithm.hpp"
 
 #include <array>
 #include <bitset>
@@ -176,121 +180,6 @@ TEST_CASE("traverse table breadth-first-search", "[bfs]")
         [&table, itr = std::begin(check)](const auto& _node) mutable
 		{
 			REQUIRE(*(itr++) == table[_node.vertex.y][_node.vertex.x]);
-		}
-    );
-}
-
-TEST_CASE("traverse dijkstra-search", "[dijkstra]")
-{
-    constexpr Vector size{ 10, 10 };
-	
-	auto table = make_table_graph<size.x, size.y>();
-
-    auto bfsNeighbourSearcher = [&table](const auto& _node, auto _callback)
-    {
-        for (int i = 0; i < 4; ++i)
-        {
-            auto cur = _node.vertex;
-            switch (i)
-            {
-            case 0: ++cur.x; break;
-            case 1: --cur.x; break;
-            case 2: ++cur.y; break;
-            case 3: --cur.y; break;
-            }
-            
-            if (0 <= cur.y && cur.y < std::size(table) &&
-                0 <= cur.x && cur.x < std::size(table[cur.y]))
-            {
-                _callback(cur);
-            }
-        }
-    };
-
-    constexpr std::array<int, size.x * size.y> check
-    {
-         0, 1,10, 2,11,20, 3,12,21,30,
-    	 4,13,22,31,40, 5,14,23,32,41,
-    	50, 6,15,24,33,42,51,60, 7,16,
-    	25,34,43,52,61,70, 8,17,26,35,
-    	44,53,62,71,80, 9,18,27,36,45,
-    	54,63,72,81,90,19,28,37,46,55,
-    	64,73,82,91,29,38,47,56,65,74,
-    	83,92,39,48,57,66,75,84,93,49,
-    	58,67,76,85,94,59,68,77,86,95,
-    	69,78,87,96,79,88,97,89,98,99
-    };
-
-	// check early return
-	sl::graph::traverse_dijkstra(Vector{ 0, 0 }, bfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-        sl::graph::ConstWeight<1>(), sl::graph::ConstWeight<0>(),
-        [count = 0](const auto&) mutable
-		{
-			REQUIRE(count == 0);
-			++count;
-			return true;
-		}
-    );
-	
-	sl::graph::traverse_dijkstra(Vector{ 0, 0 }, bfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-        sl::graph::ConstWeight<1>(), sl::graph::ConstWeight<0>(),
-        [last_weight = int(0)](const auto& _node) mutable
-		{
-			REQUIRE(last_weight <= _node.weight_sum);
-			last_weight = std::max(last_weight, _node.weight_sum);
-		}
-    );
-}
-
-TEST_CASE("traverse astar-search", "[astar]")
-{
-    constexpr Vector size{ 10, 10 };
-	
-	auto table = make_table_graph<size.x, size.y>();
-
-    auto neighbourSearcher = [&table](const auto& _node, auto _callback)
-    {
-        for (int i = 0; i < 4; ++i)
-        {
-            auto cur = _node.vertex;
-            switch (i)
-            {
-            case 0: ++cur.x; break;
-            case 1: --cur.x; break;
-            case 2: ++cur.y; break;
-            case 3: --cur.y; break;
-            }
-            
-            if (0 <= cur.y && cur.y < std::size(table) &&
-                0 <= cur.x && cur.x < std::size(table[cur.y]))
-            {
-                _callback(cur);
-            }
-        }
-    };
-
-    constexpr std::array<int, size.x * size.y> check
-    {
-         0, 1,10, 2,11,20, 3,12,21,30,
-    	 4,13,22,31,40, 5,14,23,32,41,
-    	50, 6,15,24,33,42,51,60, 7,16,
-    	25,34,43,52,61,70, 8,17,26,35,
-    	44,53,62,71,80, 9,18,27,36,45,
-    	54,63,72,81,90,19,28,37,46,55,
-    	64,73,82,91,29,38,47,56,65,74,
-    	83,92,39,48,57,66,75,84,93,49,
-    	58,67,76,85,94,59,68,77,86,95,
-    	69,78,87,96,79,88,97,89,98,99
-    };
-
-	// traverse from top-left to bottom-right
-	sl::graph::traverse_astar(Vector{ 0, 0 }, Vector{ 9, 9 }, neighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-        [](const Vector& _lhs, const Vector& _rhs) { return std::abs(_lhs.x - _rhs.x) + std::abs(_lhs.y - _rhs.y); },
-        sl::graph::ConstWeight<1>(), sl::graph::ConstWeight<0>(),
-        [last_weight = int(0), counter = int(0)](const auto& _node) mutable
-		{
-			last_weight = std::max(last_weight, _node.weight_sum);
-			//REQUIRE(++counter < 20);
 		}
     );
 }
