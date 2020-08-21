4f57176f50926ad38025ad84afbdaac8dc74baab
DNKpp <DNKpp2011@gmail.com>
2020-08-21 17:23:49 +0200

    add real test cases for Dijkstra
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/DijkstraTest.cpp
+++ test/DijkstraTest.cpp
@@ -6,307 +6,58 @@
 #include "Simple-Graph/Dijkstra.hpp"
 
 #include "catch.hpp"
-
 #include "TestUtility.hpp"
 
-//TEST_CASE("traverse table A*", "[AStar]")
-//{
-//	using Node_t = sl::graph::AStarNodeState<Vector, int>;
-//	
-//	constexpr Vector size{ 10, 10 };
-//    constexpr auto table = makeTableGraph<size.x, size.y>();
-//	
-//	//constexpr std::array<int, size.x * size.y> check
-// //   {
-// //        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-// //   	19,18,17,16,15,14,13,12,11,10,
-// //   	20,21,22,23,24,25,26,27,28,29,
-// //   	39,38,37,36,35,34,33,32,31,30,
-// //   	40,41,42,43,44,45,46,47,48,49,
-// //   	59,58,57,56,55,54,53,52,51,50,
-// //   	60,61,62,63,64,65,66,67,68,69,
-// //   	79,78,77,76,75,74,73,72,71,70,
-// //   	80,81,82,83,84,85,86,87,88,89,
-// //   	99,98,97,96,95,94,93,92,91,90
-// //   };
-//
-//	//std::map<Vector, Node_t> stateMap;
-//	sl::graph::new_traverse_astar(Vector{ 0, 0 }, Vector{ 7, 8 }, PropertyMap{ table }, NeighbourSearcher{table}/*, stateMap*/);
-//	//REQUIRE(stateMap.find(Vector{ 7, 8 })->second.parent);
-//}
-//template <int WIDTH, int HEIGHT>
-//auto make_table_graph()
-//{
-//    std::array<std::array<int, WIDTH>, HEIGHT> table;
-//    int value = 0;
-//    for (auto& row : table)
-//        std::generate(std::begin(row), std::end(row), [&value]() { return value++; });
-//    return table;
-//}
-//
-//template <int WIDTH, int HEIGHT>
-//class TableVisitationTracker
-//{
-//public:
-//    decltype(auto) operator [](const Vector& _at) const
-//    {
-//        return m_Check[_at.y * WIDTH + _at.x];
-//    }
-//    
-//    decltype(auto) operator [](const Vector& _at)
-//    {
-//        return m_Check[_at.y * WIDTH + _at.x];
-//    }
-//    
-//private:
-//    std::bitset<WIDTH * WIDTH> m_Check;
-//};
-//
-//TEST_CASE("traverse table depth-first-search", "[dfs]")
-//{
-//    constexpr Vector size{ 10, 10 };
-//	
-//	auto table = make_table_graph<size.x, size.y>();
-//
-//	auto dfsNeighbourSearcher = [&table](const auto& _node, auto _callback) -> std::optional<Vector>
-//    {
-//        for (int i = 0; i < 4; ++i)
-//        {
-//            auto cur = _node.vertex;
-//            switch (i)
-//            {
-//            case 0: ++cur.x; break;
-//            case 1: --cur.x; break;
-//            case 2: ++cur.y; break;
-//            case 3: --cur.y; break;
-//            }
-//            
-//            if (0 <= cur.y && cur.y < std::size(table) &&
-//                0 <= cur.x && cur.x < std::size(table[cur.y]))
-//            {
-//                if (_callback(cur))
-//                    return cur;
-//            }
-//        }
-//        return std::nullopt;
-//    };
-//
-//    constexpr std::array<int, size.x * size.y> check
-//    {
-//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-//    	19,18,17,16,15,14,13,12,11,10,
-//    	20,21,22,23,24,25,26,27,28,29,
-//    	39,38,37,36,35,34,33,32,31,30,
-//    	40,41,42,43,44,45,46,47,48,49,
-//    	59,58,57,56,55,54,53,52,51,50,
-//    	60,61,62,63,64,65,66,67,68,69,
-//    	79,78,77,76,75,74,73,72,71,70,
-//    	80,81,82,83,84,85,86,87,88,89,
-//    	99,98,97,96,95,94,93,92,91,90
-//    };
-//
-//	// check early returns
-//	sl::graph::traverse_dfs(Vector{ 0, 0 }, dfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//        [count = 0](const auto& _node) mutable
-//		{
-//			REQUIRE(count == 0);
-//			++count;
-//			return true;
-//		}
-//    );
-//
-//	sl::graph::traverse_dfs(Vector{ 0, 0 }, dfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//		sl::graph::EmptyCallback{},
-//        [count = 0](const auto& _node) mutable
-//		{
-//			REQUIRE(count == 0);
-//			++count;
-//			return true;
-//		}
-//    );
-//	
-//	sl::graph::traverse_dfs(Vector{ 0, 0 }, dfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//        [&table, itr = std::begin(check)](const auto& _node) mutable
-//		{
-//			REQUIRE(*(itr++) == table[_node.vertex.y][_node.vertex.x]);
-//		},
-//        [&table, itr = std::rbegin(check)](const auto& _node) mutable
-//		{
-//			REQUIRE(*(itr++) == table[_node.vertex.y][_node.vertex.x]);
-//		}
-//    );
-//}
-//
-//TEST_CASE("traverse table breadth-first-search", "[bfs]")
-//{
-//    constexpr Vector size{ 10, 10 };
-//	
-//	auto table = make_table_graph<size.x, size.y>();
-//
-//    auto bfsNeighbourSearcher = [&table](const auto& _node, auto _callback)
-//    {
-//        for (int i = 0; i < 4; ++i)
-//        {
-//            auto cur = _node.vertex;
-//            switch (i)
-//            {
-//            case 0: ++cur.x; break;
-//            case 1: --cur.x; break;
-//            case 2: ++cur.y; break;
-//            case 3: --cur.y; break;
-//            }
-//            
-//            if (0 <= cur.y && cur.y < std::size(table) &&
-//                0 <= cur.x && cur.x < std::size(table[cur.y]))
-//            {
-//                _callback(cur);
-//            }
-//        }
-//    };
-//
-//    constexpr std::array<int, size.x * size.y> check
-//    {
-//         0, 1,10, 2,11,20, 3,12,21,30,
-//    	 4,13,22,31,40, 5,14,23,32,41,
-//    	50, 6,15,24,33,42,51,60, 7,16,
-//    	25,34,43,52,61,70, 8,17,26,35,
-//    	44,53,62,71,80, 9,18,27,36,45,
-//    	54,63,72,81,90,19,28,37,46,55,
-//    	64,73,82,91,29,38,47,56,65,74,
-//    	83,92,39,48,57,66,75,84,93,49,
-//    	58,67,76,85,94,59,68,77,86,95,
-//    	69,78,87,96,79,88,97,89,98,99
-//    };
-//
-//	// check early return
-//	sl::graph::traverse_bfs(Vector{ 0, 0 }, bfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//        [count = 0](const auto& _node) mutable
-//		{
-//			REQUIRE(count == 0);
-//			++count;
-//			return true;
-//		}
-//    );
-//	
-//	sl::graph::traverse_bfs(Vector{ 0, 0 }, bfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//        [&table, itr = std::begin(check)](const auto& _node) mutable
-//		{
-//			REQUIRE(*(itr++) == table[_node.vertex.y][_node.vertex.x]);
-//		}
-//    );
-//}
-//
-//TEST_CASE("traverse dijkstra-search", "[dijkstra]")
-//{
-//    constexpr Vector size{ 10, 10 };
-//	
-//	auto table = make_table_graph<size.x, size.y>();
-//
-//    auto bfsNeighbourSearcher = [&table](const auto& _node, auto _callback)
-//    {
-//        for (int i = 0; i < 4; ++i)
-//        {
-//            auto cur = _node.vertex;
-//            switch (i)
-//            {
-//            case 0: ++cur.x; break;
-//            case 1: --cur.x; break;
-//            case 2: ++cur.y; break;
-//            case 3: --cur.y; break;
-//            }
-//            
-//            if (0 <= cur.y && cur.y < std::size(table) &&
-//                0 <= cur.x && cur.x < std::size(table[cur.y]))
-//            {
-//                _callback(cur);
-//            }
-//        }
-//    };
-//
-//    constexpr std::array<int, size.x * size.y> check
-//    {
-//         0, 1,10, 2,11,20, 3,12,21,30,
-//    	 4,13,22,31,40, 5,14,23,32,41,
-//    	50, 6,15,24,33,42,51,60, 7,16,
-//    	25,34,43,52,61,70, 8,17,26,35,
-//    	44,53,62,71,80, 9,18,27,36,45,
-//    	54,63,72,81,90,19,28,37,46,55,
-//    	64,73,82,91,29,38,47,56,65,74,
-//    	83,92,39,48,57,66,75,84,93,49,
-//    	58,67,76,85,94,59,68,77,86,95,
-//    	69,78,87,96,79,88,97,89,98,99
-//    };
-//
-//	// check early return
-//	sl::graph::traverse_dijkstra(Vector{ 0, 0 }, bfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//        sl::graph::ConstWeight<1>(), sl::graph::ConstWeight<0>(),
-//        [count = 0](const auto&) mutable
-//		{
-//			REQUIRE(count == 0);
-//			++count;
-//			return true;
-//		}
-//    );
-//	
-//	sl::graph::traverse_dijkstra(Vector{ 0, 0 }, bfsNeighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//        sl::graph::ConstWeight<1>(), sl::graph::ConstWeight<0>(),
-//        [last_weight = int(0)](const auto& _node) mutable
-//		{
-//			REQUIRE(last_weight <= _node.weight_sum);
-//			last_weight = std::max(last_weight, _node.weight_sum);
-//		}
-//    );
-//}
-//
-//TEST_CASE("traverse astar-search", "[astar]")
-//{
-//    constexpr Vector size{ 10, 10 };
-//	
-//	auto table = make_table_graph<size.x, size.y>();
-//
-//    auto neighbourSearcher = [&table](const auto& _node, auto _callback)
-//    {
-//        for (int i = 0; i < 4; ++i)
-//        {
-//            auto cur = _node.vertex;
-//            switch (i)
-//            {
-//            case 0: ++cur.x; break;
-//            case 1: --cur.x; break;
-//            case 2: ++cur.y; break;
-//            case 3: --cur.y; break;
-//            }
-//            
-//            if (0 <= cur.y && cur.y < std::size(table) &&
-//                0 <= cur.x && cur.x < std::size(table[cur.y]))
-//            {
-//                _callback(cur);
-//            }
-//        }
-//    };
-//
-//    constexpr std::array<int, size.x * size.y> check
-//    {
-//         0, 1,10, 2,11,20, 3,12,21,30,
-//    	 4,13,22,31,40, 5,14,23,32,41,
-//    	50, 6,15,24,33,42,51,60, 7,16,
-//    	25,34,43,52,61,70, 8,17,26,35,
-//    	44,53,62,71,80, 9,18,27,36,45,
-//    	54,63,72,81,90,19,28,37,46,55,
-//    	64,73,82,91,29,38,47,56,65,74,
-//    	83,92,39,48,57,66,75,84,93,49,
-//    	58,67,76,85,94,59,68,77,86,95,
-//    	69,78,87,96,79,88,97,89,98,99
-//    };
-//
-//	// traverse from top-left to bottom-right
-//	sl::graph::traverse_astar(Vector{ 0, 0 }, Vector{ 9, 9 }, neighbourSearcher, TableVisitationTracker<size.x, size.y>{},
-//        [](const Vector& _lhs, const Vector& _rhs) { return std::abs(_lhs.x - _rhs.x) + std::abs(_lhs.y - _rhs.y); },
-//        sl::graph::ConstWeight<1>(), sl::graph::ConstWeight<0>(),
-//        [last_weight = int(0), counter = int(0)](const auto& _node) mutable
-//		{
-//			last_weight = std::max(last_weight, _node.weight_sum);
-//			//REQUIRE(++counter < 20);
-//		}
-//    );
-//}
+TEST_CASE("traverse table dijkstra", "[Dijkstra]")
+{
+	constexpr Vector size{ 10, 10 };
+	std::array<std::array<int, 10>, 10> table{};
+	table.fill({ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+	
+	using DfsNodeInfo_t = sl::graph::DijkstraNodeInfo_t<Vector, int>;
+	sl::graph::DefaultDijkstraStateMap_t<Vector, int> stateMap;
+	SECTION("check early return through callback")
+	{
+		int count = 0;
+		sl::graph::traverseDijkstra(
+								Vector{ 0, 0 },
+								Vector{ 9, 9 },
+								PropertyMap{ table },
+								NeighbourSearcher{ table, false },
+								stateMap,
+								[&count](const auto& vertex, const auto& nodeInfo)
+								{
+									++count;
+									return true;
+								}
+							);
+		REQUIRE(count == 1);
+		REQUIRE(std::size(stateMap) == 1);
+		REQUIRE(std::count_if(begin(stateMap), end(stateMap), [](const auto& pair){ return pair.second.state == sl::graph::NodeState::closed; }) == 1);
+		REQUIRE(std::none_of(begin(stateMap), end(stateMap), [](const auto& pair){ return pair.second.state == sl::graph::NodeState::open; }));
+	}
+
+	SECTION("check node visitation count via callback")
+	{
+		sl::graph::traverseDijkstra(
+								Vector{ 0, 0 },
+								Vector{ 9, 9 },
+								PropertyMap{ table },
+								NeighbourSearcher{ table, false },
+								stateMap,
+								[&table](const auto& vertex, const auto& nodeInfo)
+								{
+									++table[vertex.y][vertex.x];
+								}
+							);
+		REQUIRE(
+				std::all_of(
+					begin(table),
+					end(table),
+					[](const auto& row) { return std::all_of(begin(row), end(row), [](const auto& val) { return val == 2; }); })
+				);
+		REQUIRE(std::size(stateMap) == size.x * size.y);
+		REQUIRE(std::all_of(begin(stateMap), end(stateMap), [](const auto& pair){ return pair.second.state == sl::graph::NodeState::closed; }));
+		REQUIRE(std::none_of(begin(stateMap), end(stateMap), [](const auto& pair){ return pair.second.state == sl::graph::NodeState::open; }));
+	}
+}
