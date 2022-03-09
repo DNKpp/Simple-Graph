5afd2714b6ee6b47f0d8f213b04b32ac9f876424
DNKpp <DNKpp2011@gmail.com>
2022-03-09 17:28:01 +0100

    refactor: move test helper classes into helper.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- tests/src/uniform_cost_traverse.cpp
+++ tests/src/uniform_cost_traverse.cpp
@@ -10,63 +10,9 @@
 #include "Simple-Graph/depth_first_search.hpp"
 #include "Simple-Graph/generic_traverse.hpp"
 
-#include "Simple-Vector/Vector.hpp"
-
-#include <array>
-#include <vector>
+#include "helper.hpp"
 
 using namespace sl::graph;
-
-template <class T, int VWidth, int VHeight>
-using grid2d = std::array<std::array<T, VWidth>, VHeight>;
-using vertex = sl::vec::Vector<int, 2>;
-
-struct vertex_less
-{
-	constexpr bool operator ()(const vertex& lhs, const vertex& rhs) const noexcept
-	{
-		return std::ranges::lexicographical_compare(lhs, rhs);
-	}
-};
-
-using state_map_2d = std::map<vertex, bool, vertex_less>;
-
-// prefers up > left > down > right
-template <class TGrid>
-struct grid_4way_neighbor_searcher
-{
-	const TGrid* grid{};
-
-	std::vector<vertex> operator ()(const vertex& v) const
-	{
-		std::vector<vertex> neighbors{};
-		if (v.x() < std::ssize((*grid)[0]) - 1)
-			neighbors.emplace_back(v + vertex{ 1, 0 });
-		if (v.y() < std::ssize(*grid) - 1)
-			neighbors.emplace_back(v + vertex{ 0, 1 });
-		if (0 < v.x())
-			neighbors.emplace_back(v - vertex{ 1, 0 });
-		if (0 < v.y())
-			neighbors.emplace_back(v - vertex{ 0, 1 });
-		return neighbors;
-	}
-};
-
-struct linear_graph_neighbor_searcher
-{
-	const int* begin{};
-	const int* end{};
-
-	std::vector<int> operator ()(const int& v) const
-	{
-		std::vector<int> neighbors{};
-		if (*begin < v)
-			neighbors.emplace_back(v - 1);
-		if (v + 1 < *end)
-			neighbors.emplace_back(v + 1);
-		return neighbors;
-	}
-};
 
 TEST_CASE("uniform_cost_traverse should visit all vertices of a given linear graph (order doesn't matter).", "[traverse]")
 {
