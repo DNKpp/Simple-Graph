067a328d74a398e70f05984f2b233ef86969d954
DNKpp <DNKpp2011@gmail.com>
2022-03-25 23:30:25 +0100

    refactor: use searcher approach in bfs
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- tests/src/uniform_cost_traverse.cpp
+++ tests/src/uniform_cost_traverse.cpp
@@ -1,7 +1,7 @@
-//          Copyright Dominic Koepke 2019 - 2022.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          https://www.boost.org/LICENSE_1_0.txt)
+//           Copyright Dominic Koepke 2022 - 2022.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_vector.hpp>
@@ -16,19 +16,22 @@ using namespace sl::graph;
 
 TEST_CASE("uniform_cost_traverse should visit all vertices of a given linear graph (order doesn't matter).", "[traverse]")
 {
+	using node_t = weighted_node<int, int>;
+
 	constexpr int begin{ 3 };
 	constexpr int end{ 9 };
 
 	std::vector<int> visitedVertices{};
 
-	detail::uniform_cost_traverse<weighted_node<int, int>>
+	detail::uniform_cost_traverse<node_t>
 	(
-		5,
+		detail::make_weighted_node_factory<int>(constant_weight_t<1>{}),
+		{ .vertex = 5 },
 		linear_graph_neighbor_searcher{ .begin = &begin, .end = &end },
 		[&](const auto& v) { visitedVertices.emplace_back(v.vertex); },
 		true_constant_t{},
 		std::map<int, bool>{},
-		std::stack<weighted_node<int, int>>{}
+		std::stack<node_t>{}
 	);
 
 	REQUIRE_THAT(visitedVertices, Catch::Matchers::UnorderedEquals(std::vector{ 5, 6, 7, 8, 4, 3 }));
@@ -36,18 +39,21 @@ TEST_CASE("uniform_cost_traverse should visit all vertices of a given linear gra
 
 TEST_CASE("uniform_cost_traverse should visit all vertices of a given grid.", "[traverse]")
 {
+	using node_t = weighted_node<vertex, int>;
+
 	constexpr grid2d<int, 3, 4> grid{};
 
 	int invokeCounter = 0;
 
-	detail::uniform_cost_traverse<weighted_node<vertex, int>>
+	detail::uniform_cost_traverse<node_t>
 	(
-		vertex{ 1, 1 },
+		detail::make_weighted_node_factory<vertex>(constant_weight_t<1>{}),
+		{ .vertex = { 1, 1 } },
 		grid_4way_neighbor_searcher{ &grid },
 		[&invokeCounter](const auto&) { ++invokeCounter; },
 		true_constant_t{},
 		state_map_2d{},
-		std::stack<weighted_node<vertex, int>>{}
+		std::stack<node_t>{}
 	);
 
 	REQUIRE(invokeCounter == 3 * 4);
@@ -55,19 +61,22 @@ TEST_CASE("uniform_cost_traverse should visit all vertices of a given grid.", "[
 
 TEST_CASE("uniform_cost_traverse should skip vertices for which predicate returns false (order doesn't matter).", "[traverse]")
 {
+	using node_t = weighted_node<int, int>;
+
 	constexpr int begin{ 3 };
 	constexpr int end{ 9 };
 
 	std::vector<int> visitedVertices{};
 
-	detail::uniform_cost_traverse<weighted_node<int, int>>
+	detail::uniform_cost_traverse<node_t>
 	(
-		5,
+		detail::make_weighted_node_factory<int>(constant_weight_t<1>{}),
+		{ .vertex = 5 },
 		linear_graph_neighbor_searcher{ .begin = &begin, .end = &end },
 		[&](const auto& v) { visitedVertices.emplace_back(v.vertex); },
 		[](const auto& predecessor, const int& v) { return v != 7; },
 		std::map<int, bool>{},
-		std::stack<weighted_node<int, int>>{}
+		std::stack<node_t>{}
 	);
 
 	REQUIRE_THAT(visitedVertices, Catch::Matchers::UnorderedEquals(std::vector{ 5, 6, 4, 3 }));
@@ -75,14 +84,17 @@ TEST_CASE("uniform_cost_traverse should skip vertices for which predicate return
 
 TEST_CASE("uniform_cost_traverse should exit early, if callback returns true.", "[traverse]")
 {
+	using node_t = weighted_node<int, int>;
+
 	constexpr int begin{ 3 };
 	constexpr int end{ 9 };
 	constexpr int goal{ 7 };
 
 	std::optional<int> lastVisited{};
-	detail::uniform_cost_traverse<weighted_node<int, int>>
+	detail::uniform_cost_traverse<node_t>
 	(
-		5,
+		detail::make_weighted_node_factory<int>(constant_weight_t<1>{}),
+		{ .vertex = 5 },
 		linear_graph_neighbor_searcher{ .begin = &begin, .end = &end },
 		[&](const auto& node)
 		{
@@ -91,50 +103,91 @@ TEST_CASE("uniform_cost_traverse should exit early, if callback returns true.", 
 		},
 		true_constant_t{},
 		std::map<int, bool>{},
-		std::stack<weighted_node<int, int>>{}
+		std::stack<node_t>{}
 	);
 
 	REQUIRE(lastVisited == goal);
 }
 
-TEST_CASE("traverse_dfs should visit all vertices in a specific order.", "[traverse]")
+//TEST_CASE("traverse_dfs should visit all vertices in a specific order.", "[traverse]")
+//{
+//	constexpr grid2d<int, 3, 4> grid{};
+//
+//	// provided neighbor searcher prefers up > left > down > right
+//	const std::vector<std::tuple<vertex, int>> expected_depths{
+//		{ { 0, 0 }, 1 },
+//		{ { 1, 0 }, 2 },
+//		{ { 2, 0 }, 3 },
+//
+//		{ { 0, 1 }, 0 },
+//		{ { 1, 1 }, 1 },
+//		{ { 2, 1 }, 4 },
+//
+//		{ { 0, 2 }, 1 },
+//		{ { 1, 2 }, 6 },
+//		{ { 2, 2 }, 5 },
+//
+//		{ { 0, 3 }, 8 },
+//		{ { 1, 3 }, 7 },
+//		{ { 2, 3 }, 6 }
+//	};
+//
+//	std::vector<std::tuple<vertex, int>> depths{};
+//
+//	dfs::traverse
+//	(
+//		vertex{ 0, 1 },
+//		grid_4way_neighbor_searcher{ &grid },
+//		[&](const auto& v) { depths.emplace_back(v.vertex, v.weight_sum); },
+//		true_constant_t{},
+//		state_map_2d{}
+//	);
+//
+//	REQUIRE_THAT(depths, Catch::Matchers::UnorderedEquals(expected_depths));
+//}
+
+TEST_CASE("traverse_bfs should correctly expose its typedefs.", "[bfs]")
 {
 	constexpr grid2d<int, 3, 4> grid{};
 
-	// provided neighbor searcher prefers up > left > down > right
-	const std::vector<std::tuple<vertex, int>> expected_depths{
-		{ { 0, 0 }, 1 },
-		{ { 1, 0 }, 2 },
-		{ { 2, 0 }, 3 },
-
-		{ { 0, 1 }, 0 },
-		{ { 1, 1 }, 1 },
-		{ { 2, 1 }, 4 },
-
-		{ { 0, 2 }, 1 },
-		{ { 1, 2 }, 6 },
-		{ { 2, 2 }, 5 },
-
-		{ { 0, 3 }, 8 },
-		{ { 1, 3 }, 7 },
-		{ { 2, 3 }, 6 }
+	const bfs::Searcher searcher
+	{
+		.begin = vertex{ 0, 1 },
+		.neighborSearcher = grid_4way_neighbor_searcher{ &grid },
+		.callback = empty_invokable_t{},
+		.vertexPredicate = true_constant_t{},
+		.stateMap = state_map_2d{},
+		.openList = bfs::default_open_list_t<vertex>{}
 	};
+	using searcher_t = decltype(searcher);
 
-	std::vector<std::tuple<vertex, int>> depths{};
+	REQUIRE(std::same_as<searcher_t::vertex_t, vertex>);
+	REQUIRE(std::same_as<searcher_t::weight_t, int>);
+	REQUIRE(std::same_as<searcher_t::node_t, weighted_node<vertex, int>>);
 
-	dfs::traverse
-	(
-		vertex{ 0, 1 },
-		grid_4way_neighbor_searcher{ &grid },
-		[&](const auto& v) { depths.emplace_back(v.vertex, v.weight_sum); },
-		true_constant_t{},
-		state_map_2d{}
-	);
-
-	REQUIRE_THAT(depths, Catch::Matchers::UnorderedEquals(expected_depths));
+	REQUIRE(std::same_as<searcher_t::neighbor_searcher_t, grid_4way_neighbor_searcher<grid2d<int, 3, 4>>>);
+	REQUIRE(std::same_as<searcher_t::callback_t, empty_invokable_t>);
+	REQUIRE(std::same_as<searcher_t::vertex_predicate_t, true_constant_t>);
+	REQUIRE(std::same_as<searcher_t::state_map_t, state_map_2d>);
+	REQUIRE(std::same_as<searcher_t::open_list_t, bfs::default_open_list_t<vertex>>);
 }
 
-TEST_CASE("traverse_bfs should visit all vertices in a specific order.", "[traverse]")
+TEST_CASE("Test astar traverse compiling with as much default params as possible.", "[bfs]")
+{
+	constexpr int begin{ 3 };
+	constexpr int end{ 9 };
+
+	traverse
+	(
+		bfs::Searcher
+		{
+			.begin = 5,
+			.neighborSearcher = linear_graph_neighbor_searcher{ .begin = &begin, .end = &end }
+		}
+	);
+}
+
+TEST_CASE("traverse_bfs should visit all vertices in a specific order.", "[bfs][traverse]")
 {
 	constexpr grid2d<int, 3, 4> grid{};
 
@@ -159,14 +212,15 @@ TEST_CASE("traverse_bfs should visit all vertices in a specific order.", "[trave
 
 	std::vector<std::tuple<vertex, int>> depths{};
 
-	bfs::traverse
-	(
-		vertex{ 0, 1 },
-		grid_4way_neighbor_searcher{ &grid },
-		[&](const auto& v) { depths.emplace_back(v.vertex, v.weight_sum); },
-		true_constant_t{},
-		state_map_2d{}
-	);
+	bfs::Searcher searcher
+	{
+		.begin = vertex{ 0, 1 },
+		.neighborSearcher = grid_4way_neighbor_searcher{ &grid },
+		.callback = [&](const auto& v) { depths.emplace_back(v.vertex, v.weight_sum); },
+		.vertexPredicate = true_constant_t{},
+		.stateMap = state_map_2d{}
+	};
+	traverse(std::move(searcher));
 
 	REQUIRE_THAT(depths, Catch::Matchers::UnorderedEquals(expected_depths));
 }
