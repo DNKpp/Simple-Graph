ed2906e60641b0bf7e2d80bf2327c26117225d96
DNKpp <DNKpp2011@gmail.com>
2022-03-27 22:35:51 +0200

    docs: add documentation for dfs
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-Graph/depth_first_search.hpp
+++ include/Simple-Graph/depth_first_search.hpp
@@ -16,14 +16,36 @@
 
 namespace sl::graph::dfs
 {
+	/** \addtogroup dfs
+	* @{
+	*/
+
+	/**
+	 * \brief The node type of the algorithm.
+	 */
 	template <vertex_descriptor TVertex>
 	using node_t = weighted_node<TVertex, int>;
 
+	/**
+	 * \brief The state type of the algorithm.
+	 */
 	using state_t = bool;
 
+	/**
+	 * \brief The default open list type of the algorithm.
+	 */
 	template <vertex_descriptor TVertex>
 	using default_open_list_t = std::stack<node_t<TVertex>>;
 
+	/**
+	 * \brief The searcher of the algorithm.
+	 * \tparam TVertex The vertex type.
+	 * \tparam TNeighborSearcher The neighbor searcher type.
+	 * \tparam TCallback The callback type.
+	 * \tparam TVertexPredicate The vertex predicate type.
+	 * \tparam TStateMap The state map type.
+	 * \tparam TOpenList The open list type.
+	 */
 	template <
 		vertex_descriptor TVertex,
 		neighbor_searcher_for<TVertex> TNeighborSearcher,
@@ -33,28 +55,113 @@ namespace sl::graph::dfs
 		open_list_for<node_t<TVertex>> TOpenList = default_open_list_t<TVertex>>
 	struct Searcher
 	{
+		/**
+		 * \brief Alias for the actual value type of the vertex.
+		 */
 		using vertex_t = std::remove_cvref_t<TVertex>;
+
+		/**
+		 * \brief Alias for the actual weight type of the weight.
+		 */
 		using weight_t = int;
+
+		/**
+		 * \brief Alias for the node type.
+		 */
 		using node_t = weighted_node<vertex_t, weight_t>;
 
+		/**
+		 * \brief Alias for the neighbor searcher type.
+		 */
 		using neighbor_searcher_t = TNeighborSearcher;
+
+		/**
+		 * \brief Alias for the callback type.
+		 */
 		using callback_t = TCallback;
+
+		/**
+		 * \brief Alias for the vertex predicate type.
+		 */
 		using vertex_predicate_t = TVertexPredicate;
+
+		/**
+		 * \brief Alias for the state map type.
+		 */
 		using state_map_t = std::remove_cvref_t<TStateMap>;
+
+		/**
+		 * \brief Alias for the open list type.
+		 */
 		using open_list_t = std::remove_cvref_t<TOpenList>;
 
+		/**
+		 * \brief The vertex to start the traversal.
+		 */
 		TVertex begin{};
+
+		/**
+		 * \brief The neighbor searcher to be used. Must be an invokable type, which returns a range of vertices.
+		 * \details The signature of the invocation operator should match the following:
+		 * \code
+		 * range<vertex_t> operator()(const vertex_t& current)
+		 * \endcode
+		 * where ``range<vertex_t>`` must satisfy the requirements of a ``std::ranges::input_range`` type.
+		 */
 		TNeighborSearcher neighborSearcher{};
 
+		/**
+		 * \brief The callback object to be invoked for each finalized node.
+		 * \remark If not set, an empty_invokable_t object is used.
+		 * \details The signature of the invocation operator should match the following:
+		 * \code
+		 * void operator()(const node_t& current)
+		 * \endcode
+		 *
+		 * Alternatively the invoke operator may return a boolean-convertible type, where true indicates that the algorithm shall
+		 * be aborted.
+		 */
 		TCallback callback{};
+
+		/**
+		 * \brief The predicate, which will be invoked for each discovered vertex.
+		 * \remark If not set, an true_constant_t object is used.
+		 * \details The signature of the invocation operator should match the following:
+		 * \code
+		 * bool operator()(const node_t& predecessor, const vertex_t& current)
+		 * \endcode
+		 * The predicates determines whether a vertex shall be further investigated, where false means it will be skipped.
+		 */
 		TVertexPredicate vertexPredicate{};
+
+		/**
+		 * \brief The state map prototype object.
+		 * \note If not explicitly set, a ``std::map```is used, thus the vertex type must be less-comparable.
+		 * \see state_map_for concept for the actual type requirements.
+		 */
 		TStateMap stateMap{};
+
+		/**
+		 * \brief The open list prototype object.
+		 * \note If not explicitly set, a ``std::stack`` is used.
+		 * \see open_list_for concept for the actual type requirements.
+		 * \attention As the used open list has a huge impact on the actual behaviour of the algorithm, the type of the provided
+		 * open list should model the behaviour of a stack-like container. Otherwise this result in undefined behaviour.
+		 */
 		TOpenList openList{};
 	};
+
+	/** @}*/
 }
 
 namespace sl::graph
 {
+	/**
+	 * \brief Overload for dfs::searcher.
+	 * \tparam TArgs Template arguments for the searcher.
+	 * \param searcher The searcher object.
+	 * \ingroup dfs
+	 */
 	template <class... TArgs>
 	void traverse(dfs::Searcher<TArgs...> searcher)
 	{
