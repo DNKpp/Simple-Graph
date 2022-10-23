3387fefcbd70067456235d6a0885a2f93ca596e1
DNKpp <dnkpp2011@gmail.com>
2022-10-23 13:50:01 +0200

    fix: please gcc < 11.2

--- tests/src/searcher.cpp
+++ tests/src/searcher.cpp
@@ -88,16 +88,24 @@ TEST_CASE("make_searcher creates correct searcher object for basic node types.",
 {
 	using searcher = decltype(make_searcher<single_element_algorithm_def>(std::declval<IntRangeGraph>(), {}));
 
-	STATIC_REQUIRE(std::is_same_v<searcher::vertex_type, int>);
-	STATIC_REQUIRE(std::is_same_v<searcher::node_type, node<int>>);
+	STATIC_REQUIRE(std::same_as<searcher::vertex_type, int>);
+	STATIC_REQUIRE(std::same_as<searcher::node_type, node<int>>);
 }
 
 TEST_CASE("make_searcher creates correct searcher object for weighted node types.", "[searcher]")
 {
-	using searcher = decltype(make_searcher<single_element_weighted_algorithm_def>(std::declval<IntRangeWeightedGraph>(), {}));
+	// I actually have no idea, why gcc below 11.2 has an issue with the general approach.
+#if (__GNUC__ < 11 || (__GNUC__ == 11 && __GNUC_MINOR__ < 2))
+	using searcher = detail::searcher_type<
+			single_element_weighted_algorithm_def<graph_vertex_type<IntRangeWeightedGraph>, graph_weight_type<IntRangeWeightedGraph>>,
+			IntRangeWeightedGraph
+	>;
+#else
+	using searcher = decltype(make_searcher<single_element_weighted_algorithm_def>(std::declval<const IntRangeWeightedGraph&>(), 0));
+#endif
 
-	STATIC_REQUIRE(std::is_same_v<searcher::vertex_type, int>);
-	STATIC_REQUIRE(std::is_same_v<searcher::node_type, weighted_node<int, int>>);
+	STATIC_REQUIRE(std::same_as<searcher::vertex_type, int>);
+	STATIC_REQUIRE(std::same_as<searcher::node_type, weighted_node<int, int>>);
 }
 
 TEST_CASE("searcher traverses graph and keeps algorithm state.", "[searcher]")
